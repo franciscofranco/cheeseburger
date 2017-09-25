ebe80d83015d0590d6c86ff4dcc3d01799d8fc44
Todd Kjos <tkjos@google.com>
2017-09-25 08:55:09 -0700

    FROMLIST: binder: fix use-after-free in binder_transaction()
    (from https://patchwork.kernel.org/patch/9978801/)
    
    User-space normally keeps the node alive when creating a transaction
    since it has a reference to the target. The local strong ref keeps it
    alive if the sending process dies before the target process processes
    the transaction. If the source process is malicious or has a reference
    counting bug, this can fail.
    
    In this case, when we attempt to decrement the node in the failure
    path, the node has already been freed.
    
    This is fixed by taking a tmpref on the node while constructing
    the transaction. To avoid re-acquiring the node lock and inner
    proc lock to increment the proc's tmpref, a helper is used that
    does the ref increments on both the node and proc.
    
    Bug: 66899329
    Change-Id: Iad40e1e0bccee88234900494fb52a510a37fe8d7
    Signed-off-by: Todd Kjos <tkjos@google.com>
    Signed-off-by: Francisco Franco <franciscofranco.1990@gmail.com>

--- drivers/android/binder.c
+++ drivers/android/binder.c
@@ -2930,7 +2930,7 @@ static void binder_transaction(struct binder_proc *proc,
 				return_error = BR_DEAD_REPLY;
 			mutex_unlock(&context->context_mgr_node_lock);
 		}
-		if (target_node == NULL) {
+		if (!target_node) {
 			/*
 			 * return_error is set above
 			 */
